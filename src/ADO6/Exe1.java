284d5799254292ed683b50e614688a43f7ca5fa2
Nicolas dos Santos Andrade <nicolas.andrade@compasso.com.br>
2023-03-25 00:59:30 -0300

    ADO6 inicio da atividade

---
+++ src/ADO6/Exe1.java
@@ -0,0 +1,24 @@
+package ADO6;
+
+import java.util.Scanner;
+
+public class Exe1 {
+    public static void main(String[] args) {
+
+        Scanner resposta = new Scanner(System.in);
+
+        String alternativa;
+
+        do {
+
+            alternativa = resposta.next();
+
+            switch (alternativa){
+
+
+            }
+
+        } while (alternativa != "b");
+
+    }
+}
