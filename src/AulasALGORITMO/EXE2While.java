b7d6f14f0fc0cf66bc08e8041b7060e0df0e155b
Nicolas dos Santos Andrade <nicolas.andrade@compasso.com.br>
2023-03-27 21:49:41 -0300

    atividade 2 algoritmos

--- src/AulasALGORITMO/EXE2While.java
+++ src/AulasALGORITMO/EXE2While.java
@@ -1,7 +1,21 @@
 package AulasALGORITMO;
 
+import java.util.Scanner;
+
 public class EXE2While {
     public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)){
+            int numero, soma = 0;
+
+            do {
+                System.out.println("Digite um número ");
+                numero = entrada.nextInt();
+                soma += numero;
+
+            }while (numero != 0);
+            System.out.println("A soma dos números digitados é: " + soma);
+
+        }
 
     }
 }
